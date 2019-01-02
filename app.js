8d477967d1502108bec5770dad83f97c714a9a3a
James Chen <jameschen1295@gmail.com>
2019-01-02 12:01:35 -0800

    restful routing setup

--- app.js
+++ app.js
@@ -18,11 +18,102 @@ app.use(methodOverride("_method"));
 // Connect database
 mongoose.connect("mongodb://james:james1@ds147684.mlab.com:47684/health-app", {useNewUrlParser: true});
 
+// CATEGORY SCHEMA
+var categorySchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  members: String,
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post"
+    }
+  ]
+});
+var Category = mongoose.model("Category", categorySchema);
+
+// POST SCHEMA
+var postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  create: {type: Date, default: Date.now},
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  }
+});
+var Post = mongoose.model("Post", postSchema);
+
+// COMMENTS SCHEMA
+var commentSchema = mongoose.Schema({
+  text: String,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  username: String,
+  profilePicture: String
+});
+var Comment = mongoose.model("Comment", commentSchema);
+
 // ROUTES
 app.get("/", function(req, res){
-  res.render("home");
+  res.redirect("home");
 });
 
+// RESTFUL ROUTES
+// INDEX
+app.get("/home", function(req, res){
+  Category.find({}, function(err, categories){
+    if(err){
+      res.redirect("home");
+    } else {
+      res.render("homePage", {categories: categories});
+    }
+  });
+});
+
+// NEW ROUTE
+app.get("/home/new", function(req, res){
+  res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/home", function(req, res){
+  Category.create(req.body.category, function(err, newCategory){
+    if(err){
+      res.render("new");
+    } else {
+      res.redirect("/home");
+    }
+  });
+});
+
+// SHOW ROUTE
+app.get("/home/:id", function(req, res){
+  Category.findById(req.params.id, function(err, currCategory){
+    if(err){
+      res.redirect("/home");
+    } else {
+      res.render("show", {category: currCategory});
+    }
+  });
+});
 
 // LISTENERS
 // HEROKU
